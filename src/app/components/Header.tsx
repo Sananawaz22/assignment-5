e236c5da8a694dd0806224c8db6e6616e3a80583
Sananawaz32 <Sananawaz984@gmail.com>
2024-12-14 14:43:45 +0500

    first commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="h-[80px] bg-[#A29875] flex item-center justify-center md:justify-start">
+    <div className="text-[30px] md:text-[40px] text-[#FFFFFF] md:ml-10x1:text-[75px]" style={{ fontFamily:'Ray,sans-serif',fontWeight: 300}}>MANZZARI</div>
+
+    
+    </div>
+  )
+}
+
+export default Header
